3a46e876daaf0e11b6cdd7f4fb86b804a769786e
ANISH-DON <113435227+ANISSSSSH@users.noreply.github.com>
2024-04-24 18:51:27 +0545

    Add files via upload

---
+++ xyz.py
@@ -0,0 +1,175 @@
+6113431|Prabin Paudel
+17828679|Prabol Bhandari
+9352484|Chantelle Jay
+157400358|Jagreet Khanal
+157400383|Suraj Lohani
+11019327|Rachel McLaughlin
+157400548|नेपाली वर्षा बिष्ट
+13908078|Saroj Aryal
+178002508|Suman Cha
+26802809|Abia Guyana
+183900927|Preeyanka Parajuli
+26901614|Hayaana Marine
+500472972|Nitesh Timilsina
+42205892|Cameron Allen-Jones
+500596524|Dipesh Chhetri
+42411420|Julia Teresa
+500813243|Sandesh Shrestha
+44014441|Sayudh Mainalee
+49706591|Suresh Regmi
+500864997|Nischeet Shrestha
+50609791|Unnat Indu Dhital
+77400868|Brian Cheeks
+77404334|Andrew Toweh
+501061060|Rudra Kafle
+80603712|Te' Kola Howard
+501286061|Sudeep Kc
+113300830|Teresa McLorn
+501654872|Raju Sherchan
+145300218|Jamaal Carrington
+501803968|Ashim Raj Pathak
+502072753|Amit Raj Satyal
+146800225|Victoria Wilson
+502328025|Ram Basnet
+187001301|Kevin Bryan
+502630002|Sandesh Katwal
+203803452|Joshua Watson
+502743169|Ranendra Pratap Malla
+224100117|SJ Aryal
+503217907|Sarad Poudyal
+500069252|Deene Pace
+504662218|Ashish Chhetry
+500088882|Rich Carter
+8370789|Netra Regmi
+16739637|Manju Sigdel
+502083705|Prince Okafor
+505445068|Basnet Kamana
+28704615|Hem Joshi
+506604539|Aabid Mohd
+503333343|Prakash Pokhrel
+502352304|Dipendra Lamichhane
+506665005|Sudeep Uprety
+503911532|Kalyan Nepal
+507448559|Anish Khanal
+502499661|Raj Prithvi Aryal
+507859204|Aakriti Kharel
+506426765|Narayan Paudel
+120401151|Gareth Jones
+516729441|Bimal Nepal
+500742652|Susan Chhetri
+518338365|Sudeep Raj Aryal
+507271080|Jocelyn Closs
+520968858|Saroj Nepal
+509001554|Kelsie Marie
+522597940|Megharaj Bhandari
+511632624|Garth James Coxford
+524713648|Durga Khanal Regmi
+521030056|Holly Mac
+525046355|Parishma Jangam
+530582094|Doc Tor
+563015691|Shelley Horner
+528280294|Achyut Adhikari
+609217470|Erin Doepker
+528310223|Naresh Dangol
+530210300|Homnath Subedi
+642440216|Geoff Weisbeck
+540176211|Laxman Pradhan
+656302712|आदर्श सिङ
+550846007|Karin Hillen
+675239458|Susie Mah
+714523151|KA-ZI BRAND カジ
+734020304|Shane Tylosky
+553008607|गणेश तिमल्सेना
+740050938|Michelle Thorgeirson
+745375511|Gabriel Fortin
+557559658|Ritesh Adhikari
+785705367|Sʌnoj Don
+571500551|Muna Paudel
+846610083|Jeanne Fagan
+872075787|Gerald Wingerter
+572052102|Ganesh Prasad Pandeya
+579028156|Rudra Budhathoki
+873200220|Erin Cuthbert Bugbee
+1006568898|Denise Miller
+579500510|Subir Pradhan
+1345096180|Rahool Karkey
+581261700|Sunil Mani Dahal
+1367447776|Ma Riø
+581776544|Prashanna Sangroula
+1826953347|Dazaii Kai
+61550035266965|Hikmat Bk
+500742652|Susan Chhetri
+1551994081|Robben Katwal
+61550487971252|Shristy Ghimire
+61550694590606|Riya Thapa
+61550780736741|Sansila Koirala
+61551060442931|Dipchan Magar
+61551582021222|ۦۦ SAMEER
+61552805016786|ईशाा मगर भुजेल
+61553161243019|Krishna Katuwal
+61553557408528|Dipak Bhujel
+61554058964550|Ankit Dhakal
+61555081133305|Yu Na
+61555664672065|Mallika Sapkota
+100000091465968|SAILESH　 ゎろゎ
+100000178080443|Saw Roj
+100000286923316|Avik Poudel
+100000500826743|गौतम लामा
+9807340|Deepesh Vaidya
+100000631334725|A Bee Simkhada
+505886314|Sudeep Raymajhi
+100000686986373|Chetan Subedi
+514247507|Ankur AD
+515264485|Onisha PS
+517631274|Ajit Baral
+518538801|Yukta Narayan Regmi
+100000986531501|Sandip愛
+100001209683241|महेश शर्मा
+519265677|Vipin V Pangeni
+100001432685089|Drona Sigdel
+524891006|Bhuwan Panta
+100001437032035|RajKumar Stha Raju
+528863553|Subash Banstola
+530348585|Gopikrishna Regmi
+100001684441643|Patharyodash Pramil
+533680728|Gopal K Acharya
+100001919812413|PrajZual PauDyal
+534147609|Niraj Giri
+536168156|Murahari Parajuli
+537880581|Bnay Regmi
+539425190|Shiva Karki
+540869632|Bishow Khadka
+546248686|Rabi Regmi
+547159901|Srijana Regmi
+547870557|Anup Baral
+551514650|Prabhakar Khand
+558741223|Mithlesh Chaudhary
+564578724|Arjun Gautam
+565009730|Anjana Banstola Paudel
+570106115|Bijaya Ghimire
+572389404|Jeevan Gurung
+48910424|Alexandrine Al Hakim
+505019322|Ahmed Moghis
+505391304|Sikander Zulqurnain
+509954908|Udhav Khanna
+512597429|Gong Chen
+517272365|Joker Hakim
+519470984|Claudette Khoury Doumit
+520461541|Poss Palmer
+522505801|Bassam Nachef
+523001985|Wadih Farah
+525659444|Abu Rashed
+526175226|Baider Rahmatalla
+526551380|Gebran Youssef Boueri
+527543611|Mohamed Ali Elomda
+527801735|Ronald Daher
+528112439|Charlie Zeng
+529150730|Shaggy Bdair
+529517418|Toni Jabbour
+529590125|George Nouh
+530082804|Ibrahim Buafra
+530300361|Oliver Fahed
+533063482|Mickey Kemzo
+533594085|Mohammad Wahbeh
+537310028|Samer El Sayed
+539225900|Ahmed Rabie
